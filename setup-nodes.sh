622553f3b41a784d32f183a6a0738058dc7c53fa
m-eduard <marineduardconstantin2001@gmail.com>
2024-05-12 20:52:30 +0300

    Add all configurations

---
+++ setup-nodes.sh
@@ -0,0 +1,10 @@
+setup-nodes.sh
+
+# docker swarm init --advertise-addr 192.168.0.28
+# docker swarm join --token SWMTKN-1-5zuuhyixwjdatjsjlmu1250wgj9oad1h2oeilcbjeeu9r8sk01-5pq7g8g1sful6x4q0ohzr3uyn 192.168.0.28:2377
+
+# docker node ls
+
+# docker stack deploy -c docker-compose.yml proiect
+
+# docker stack rm proiect
