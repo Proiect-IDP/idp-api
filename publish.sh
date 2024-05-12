f08acf0b4fa6fd0f19aa7b824f6ca34e7e08bdfa
m-eduard <marineduardconstantin2001@gmail.com>
2024-05-12 10:08:09 +0300

    Change confiig for Swarm

--- publish.sh
+++ publish.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-image_names=("business-logic" "auth-service" "io-service" "prediction-service")
+image_names=("business-logic-service" "auth-service" "io-service" "prediction-service")
 
 HUB_USERNAME="meduard"
 docker login -u "${HUB_USERNAME}" -p "${HUB_PASSWORD}"
 
 for i in ${image_names[@]}
 do
+    cd "$i"
     docker build -t $i .
     docker tag $i:latest $HUB_USERNAME/$i:latest
     docker push $HUB_USERNAME/$i:latest
+    cd -
 done
